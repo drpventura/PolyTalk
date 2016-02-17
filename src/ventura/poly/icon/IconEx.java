1ce2c3202548b426244eeffc492bc0e701ae7c3a
Phil Ventura, Ph.D <venturap@palmbeachstate.edu>
2016-02-17 15:48:54 -0500

    Added icon examples.

--- src/ventura/poly/icon/IconEx.java
+++ src/ventura/poly/icon/IconEx.java
@@ -3,19 +3,24 @@ package ventura.poly.icon;
 import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 
+/**
+ * Based on code taken from Horstmann, C. S. (2006). Object-oriented 
+ * design & patterns (2nd ed.). Hoboken, NJ: Wiley. (p. 138)
+ *
+ */
 public class IconEx {
 
 	public static void main(String[] args) {
-//		JOptionPane.showMessageDialog(null, 
-//				"Hello, world!",
-//				"Important Message from JOptionPane",
-//				JOptionPane.ERROR_MESSAGE,
-//				new ImageIcon("DukeWithHelmet.png"));
+		JOptionPane.showMessageDialog(null, 
+				"ImageIcon",
+				"ImageIcon Demo",
+				JOptionPane.ERROR_MESSAGE,
+				new ImageIcon(IconEx.class.getResource("/images/CC.jpg")));
+		
 		JOptionPane.showMessageDialog(null, 
 				"Hello, world!",
-				"Important Message from JOptionPane",
+				"Custom Icon Demo",
 				JOptionPane.ERROR_MESSAGE,
-				new MarsIcon(130));
-
+				new MarsIcon(50));
 	}
 }
--- src/ventura/poly/icon/MarsIcon.java
+++ src/ventura/poly/icon/MarsIcon.java
@@ -4,10 +4,16 @@ import java.awt.Color;
 import java.awt.Component;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.geom.Ellipse2D;
 
 import javax.swing.Icon;
 
+/**
+ * Code from Horstmann, C. S. (2006). Object-oriented design & 
+ * patterns (2nd ed.). Hoboken, NJ: Wiley. (p. 138)
+ *
+ */
 public class MarsIcon implements Icon {
 
 	private int _size;
@@ -19,6 +25,8 @@ public class MarsIcon implements Icon {
 	@Override
 	public void paintIcon(Component c, Graphics g, int x, int y) {
 		Graphics2D g2 = (Graphics2D) g;
+		// turn on anti-aliasing
+		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 		Ellipse2D.Double planet = new
 				Ellipse2D.Double(x, y, _size, _size);
 		g2.setColor(Color.RED);
