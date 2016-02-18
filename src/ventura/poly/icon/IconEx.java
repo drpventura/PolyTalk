988344bb9dc58e19c6754f9e8c31c4e348cd9154
Philip R. Ventura, Ph.D <venturap@palmbeachstate.edu>
2016-02-18 02:07:07 -0500

    Added basic show message exmaple, changed message type.

--- src/ventura/poly/icon/IconEx.java
+++ src/ventura/poly/icon/IconEx.java
@@ -11,16 +11,18 @@ import javax.swing.JOptionPane;
 public class IconEx {
 
 	public static void main(String[] args) {
-		JOptionPane.showMessageDialog(null, 
-				"ImageIcon",
-				"ImageIcon Demo",
-				JOptionPane.ERROR_MESSAGE,
-				new ImageIcon(IconEx.class.getResource("/images/CC.jpg")));
+		JOptionPane.showMessageDialog(null, "Hello, World!");
 		
+//		JOptionPane.showMessageDialog(null, 
+//				"ImageIcon",
+//				"ImageIcon Demo",
+//				JOptionPane.INFORMATION_MESSAGE,
+//				new ImageIcon(IconEx.class.getResource("/images/CC.jpg")));
+//		
 //		JOptionPane.showMessageDialog(null, 
 //				"Hello, world!",
 //				"Custom Icon Demo",
-//				JOptionPane.ERROR_MESSAGE,
+//				JOptionPane.INFORMATION_MESSAGE,
 //				new MarsIcon(50));
 	}
 }
